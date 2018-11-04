2f8ca6d699875ed7b23996e5c0d820d801680443
muttaki1251998 <muttaki4989@gmail.com>
2018-11-05 02:00:45 +0600

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component } from 'react';
+import './App';
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <h1>Auth app</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
